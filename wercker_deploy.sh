592ecad50893e3a96a96ed60d505052fd0d2e6ad
Guilherme Nunes <guinunesilva@gmail.com>
2015-05-18 15:35:49 -0300

    wercker deploy

---
+++ wercker_deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo $WERCKER_DEPLOYTARGET_NAME
+
+#if [ "$WERCKER_DEPLOYTARGET_NAME" = "dev" ]; then
+	#sed -i 's!192.168.56.101!test.hangoutsplay.com!g' app.xml
+	scp -i $PRIVATEKEY_PATH app.xml root@104.236.20.173:/var/www/whatsdice.com
+#fi
